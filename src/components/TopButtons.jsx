3631cb7aa0ff2d2a74e64014e44068531d46dfd7
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2023-05-26 21:29:26 +0530

    first commit

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "Delhi",
+    },
+    {
+      id: 2,
+      title: "Mumbai",
+    },
+    {
+      id: 3,
+      title: "Agra",
+    },
+    {
+      id: 4,
+      title: "Mathura",
+    },
+    {
+      id: 5,
+      title: "Srinagar",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-[#00FFCA] text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
